2f0ba1f69507647b458f67ec44cb55ca9c73cb13
Sulthan Abiyyu <zblackjack980@gmail.com>
2023-06-03 20:36:51 +0700

    fixed: navbar links

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -5,8 +5,131 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Keranjank!</title>
+        <style>
+        body{
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            max-width: none;
+        }
+
+        .container {
+            width: 100%;
+            max-width: none;
+        }
+
+        .banner-container{
+            display: flex;
+            justify-content: center;
+            background-color: gray;
+            border-radius: 16px;
+            width: calc(100% - 64px);
+            max-width: none;
+            margin: 32px;
+        }
+
+        .banner{
+            max-width: 50%;
+            height: auto;
+        }
+
+        .daftar-product-title{
+            margin: 16px;
+        }
+
+        .row {
+            display: flex;
+            flex-wrap: wrap;
+        }
+
+        .col-md-4 {
+            width: calc(25%);
+            max-width: none;
+        }
+
+        .card {
+            /* border: 1px solid #ccc; */
+            box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+            border-radius: 8px;
+            padding: 12px;
+            margin: 16px;
+            background-color: #f9f9f9;
+        }
+
+        .card-body {
+            flex: content;
+            flex-direction: column;
+            justify-content: flex-start;
+        }
+
+        .card-img-top {
+            width: 100%;
+            height: 200px;
+            object-fit: cover;
+            border-radius: 5px;
+        }
+
+        .card-title {
+            font-size: 20px;
+            font-weight: bold;
+        }
+
+        .card-text {
+            font-size: 16px;
+            margin-bottom: 32px;
+        }
+
+        .card-price {
+            font-size: 20px;
+            color: chocolate;
+        }
+
+        .btn-primary {
+            background-color: #007bff;
+            color: #fff;
+            margin-bottom: 16px;
+            padding: 8px 16px;
+            border-radius: 8px;
+            text-decoration: none;
+        }
+
+        .btn-primary:hover {
+            background-color: #0056b3;
+        }
+
+        .navbar {
+            box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            width: 100%;
+            max-width: none;
+            height: 72px;
+        }
+
+        .navbar-icon {
+            height: 72px;
+        }
+
+        .navbar-menu{
+            display: flex;
+            flex-direction: row;
+        }
+
+        .navbar-menu-item{
+            margin: 8px;
+        }
+    </style>
 </head>
 <body>
+    <div class="navbar">
+            <a href="/product">
+                            <img class="navbar-icon" src="../logo_rigthttext.jpg" alt="" />
+            </a>
+            <div class="navbar-menu">
+                <a class = "navbar-menu-item" href="/cart">Keranjang</a>
+            </div>
+        </div>
     <h1>Keranjang</h1>
     <table>
         <th>No.</th>

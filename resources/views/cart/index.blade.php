7de3ca10d80ec010b04a6d0db4132827d003cf22
Sulthan Abiyyu <zblackjack980@gmail.com>
2023-06-03 20:45:59 +0700

    style: add a lil styling on cart index

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -4,86 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Keranjank!</title>
+    <title>Keranjang</title>
         <style>
-        body{
-            margin: 0;
-            padding: 0;
-            width: 100%;
-            max-width: none;
-        }
-
-        .container {
-            width: 100%;
-            max-width: none;
-        }
-
-        .banner-container{
-            display: flex;
-            justify-content: center;
-            background-color: gray;
-            border-radius: 16px;
-            width: calc(100% - 64px);
-            max-width: none;
-            margin: 32px;
-        }
-
-        .banner{
-            max-width: 50%;
-            height: auto;
-        }
-
-        .daftar-product-title{
-            margin: 16px;
-        }
-
-        .row {
-            display: flex;
-            flex-wrap: wrap;
-        }
-
-        .col-md-4 {
-            width: calc(25%);
-            max-width: none;
-        }
-
-        .card {
-            /* border: 1px solid #ccc; */
-            box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-            border-radius: 8px;
-            padding: 12px;
-            margin: 16px;
-            background-color: #f9f9f9;
-        }
-
-        .card-body {
-            flex: content;
-            flex-direction: column;
-            justify-content: flex-start;
-        }
-
-        .card-img-top {
-            width: 100%;
-            height: 200px;
-            object-fit: cover;
-            border-radius: 5px;
-        }
-
-        .card-title {
-            font-size: 20px;
-            font-weight: bold;
-        }
-
-        .card-text {
-            font-size: 16px;
-            margin-bottom: 32px;
-        }
-
-        .card-price {
-            font-size: 20px;
-            color: chocolate;
-        }
-
         .btn-primary {
             background-color: #007bff;
             color: #fff;
@@ -119,6 +41,17 @@
         .navbar-menu-item{
             margin: 8px;
         }
+
+        table {
+            border-collapse: collapse;
+        }
+        table, th, td {
+            border: 1px solid;
+        }
+        th, td, tr {
+            text-align: center;
+
+        }
     </style>
 </head>
 <body>
@@ -139,7 +72,7 @@
 
         @foreach ($products as $item)
             <tr>
-                <td>{{ $item->id }}</td>
+                <td>{{ $loop->iteration }}</td>
                 <td>{{ $item->price }}</td>
                 <td>{{ $item->name }}</td>
                 <td>
@@ -156,7 +89,7 @@
 
     <form action="{{ route('cart.checkout') }}" method="GET">
         @csrf
-        <input type="submit" value="Checkout">
+        <input class="btn-primary" type="submit" value="Checkout">
     </form>
 </body>
 </html>
